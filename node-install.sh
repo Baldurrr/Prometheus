b8bab15df3a62c097d0c22d8ad64b43251b2b2e0
Baldurrr <77190420+Baldurrr@users.noreply.github.com>
2021-02-01 18:45:00 +0100

    Update node-install.sh
    credit to haibraid

--- node-install.sh
+++ node-install.sh
@@ -6,26 +6,44 @@ groupadd --system prometheus
 echo -e "Creating user prometheus \n"
 useradd -s /sbin/nologin --system -g prometheus prometheus
 
-wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
+wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz -P /tmp
 
 echo -e "Extracting node exporter \n"
-tar xvf node_exporter-*linux-amd64.tar.gz
-cd node_exporter*/
+tar xvf /tmp/node_exporter-*linux-amd64.tar.gz -C /tmp/
+cd /tmp/node_exporter*/
 
 echo -e "Moving node exporter config in /usr/local/bin/ \n"
-mv node_exporter /usr/local/bin
-
-cd ..
+cp /tmp/node_exporter*/node_exporter /usr/local/bin
 
 echo -e "Creating node_exporter.service \n"
-mv node-config.txt /etc/systemd/system/node_exporter.service
 
+tee /etc/systemd/system/node_exporter.service<<EOF
+[Unit]
+Description=Prometheus
+Documentation=https://github.com/prometheus/node_exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+Type=simple
+User=prometheus
+Group=prometheus
+ExecReload=/bin/kill -HUP $MAINPID
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+echo -e "Reloading daemons"
+systemctl daemon-reload
 echo -e "Starting node service \n"
 systemctl start node_exporter
 echo -e "Enabling node service \n"
 systemctl enable node_exporter
 
-# Si besoin d'ouvrir un port =>
+# Open specific port with iptables
 # echo -e "Opening port 9100 \n" 
 # iptables -A INPUT -p tcp -m tcp --dport 9100 -j ACCEPT
 # iptables -A OUTPUT -p tcp -m tcp --dport 9100 -j ACCEPT
@@ -35,7 +53,4 @@ systemctl enable node_exporter
 # mv iptables.txt /etc/network/if-pre-up.d/iptables
 # chmod +x /etc/network/if-pre-up.d/iptables
 echo "End of configuration"
-rm -rf node_exporter*/
-rm node_exporter-1.0.1.linux-amd64.tar.gz
-
 
